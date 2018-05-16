0b1ad06df8338698bc9d20092143057c9e52a85b
pansinisalvatore <rino.96@hotmail.it>
2018-05-16 16:33:05 +0200

    Initial commit

---
+++ app/src/main/java/uk/co/maxcarli/carpooling/Splash_activity.java
@@ -0,0 +1,13 @@
+package uk.co.maxcarli.carpooling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+    }
+}
