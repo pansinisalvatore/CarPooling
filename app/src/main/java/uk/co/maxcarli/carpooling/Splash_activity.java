604408f3a64b8d797893ba5dc98f5ef3cf9e3f41
matera18 <33933065+matera18@users.noreply.github.com>
2018-05-18 14:34:17 +0200

    splash activity completata (forse)

--- app/src/main/java/uk/co/maxcarli/carpooling/Splash_activity.java
+++ app/src/main/java/uk/co/maxcarli/carpooling/Splash_activity.java
@@ -1,14 +1,101 @@
 package uk.co.maxcarli.carpooling;
 
 //rino
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
-public class Splash_activity extends AppCompatActivity {
+public class Splash_activity extends Activity {
+    private static final String TAG_LOG = Splash_activity.class.getName();
+    private static final long MIN_WAIT_INTERVAL= 1500L;
+    private static final long MAX_WAIT_INTERVAL= 3000L;
+    private static final int GO_AHEAD_WHAT=1;
+    private long mStartTime;
+    private boolean mIsDone;
+
+    private Handler mHandler = new Handler(){ /* metodo per la ricezione e l'invio di messaggi*/
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case GO_AHEAD_WHAT:
+                    long elapsedTime = SystemClock.uptimeMillis()- mStartTime;
+                    if (elapsedTime >= MIN_WAIT_INTERVAL && !mIsDone){
+                        mIsDone= true;
+                        /*goAhead();*/
+                    }
+                    break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_activity);
+        View decorView = getWindow().getDecorView();
+        int uiOptions= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
     }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mStartTime= SystemClock.uptimeMillis();
+        final Message goAheadMessage = mHandler.obtainMessage(GO_AHEAD_WHAT);
+        mHandler.sendMessageAtTime(goAheadMessage, mStartTime + MAX_WAIT_INTERVAL);
+        Log.d(TAG_LOG, "Handler message sent!");
+    }
+
+    private void goAhead(){
+        final Intent intent = new Intent(this, LoginActivity.class);
+        while(true){
+            if(haveInternetConnection(Splash_activity.this)){
+                startActivity(intent);
+                finish();
+            }else{
+                AlertDialog.Builder miaAlert = new AlertDialog.Builder(this);
+                miaAlert.setTitle("Connessione assente");
+                miaAlert.setMessage("Devi attivare la connessione per utilizzare l'applicazione");
+                AlertDialog alert = miaAlert.create();
+                alert.show();
+            }
+        }
+
+    }
+
+
+
+
+        public static boolean haveInternetConnection(Context contesto) {
+            boolean haveConnectedWifi = false;
+            boolean haveConnectedMobile = false;
+
+            ConnectivityManager cm = (ConnectivityManager) contesto.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+            for (NetworkInfo ni : netInfo) {
+                if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                    if (ni.isConnected())
+                        haveConnectedWifi = true;
+                if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                    if (ni.isConnected())
+                        haveConnectedMobile = true;
+            }
+            return haveConnectedWifi || haveConnectedMobile;
+        }
+
+
+
+
+
 }
