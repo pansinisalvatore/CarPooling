0a4522b1ea4eead7912928f686a601e469b7aa22
Gianni996 <giammaria.massafra@gmail.com>
2018-05-21 15:41:18 +0200

    menu a scorrimento

---
+++ app/src/main/java/uk/co/maxcarli/carpooling/ImieiPassaggi.java
@@ -0,0 +1,33 @@
+package uk.co.maxcarli.carpooling;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ImieiPassaggi extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imiei_passaggi);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.imieipassaggi);
+        mToggle=new ActionBarDrawerToggle(this, mDrawerLayout,  R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
