2f778d27e487f13150d30b9dbeb92a726cd38197
matera18 <33933065+matera18@users.noreply.github.com>
2018-05-21 15:59:42 +0200

    splash activity completata (forse)

---
+++ app/src/main/java/uk/co/maxcarli/carpooling/Principale.java
@@ -0,0 +1,34 @@
+package uk.co.maxcarli.carpooling;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Principale extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principale);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.principale);
+        mToggle=new ActionBarDrawerToggle(this, mDrawerLayout,  R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
