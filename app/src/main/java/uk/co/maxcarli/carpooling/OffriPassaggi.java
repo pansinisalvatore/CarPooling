0440310d522452134ba5f886741f1b8d7b70ebd5
domenicoitps <domenicofasciano1996@libero.it>
2018-05-28 16:51:47 +0200

    modifica offri passaggi

--- app/src/main/java/uk/co/maxcarli/carpooling/OffriPassaggi.java
+++ app/src/main/java/uk/co/maxcarli/carpooling/OffriPassaggi.java
@@ -18,18 +18,16 @@ import java.text.DateFormat;
 import java.util.Calendar;
 
 public class OffriPassaggi extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
-    Button imp_data;
-    Button imp_ora;
-    TextView set_data;
-    TextView set_ora;
+    EditText imp_data;
+    EditText imp_ora;
     EditText set_auto;
     Spinner set_posti;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offri_passaggi);
-        imp_data= findViewById(R.id.btnData);
-        imp_ora= findViewById(R.id.btnOra);
+        imp_data= findViewById(R.id.edtData);
+        imp_ora= findViewById(R.id.edtOra);
         set_auto= findViewById(R.id.edtInserisciAuto);
         set_posti= findViewById(R.id.spnPosti);
         ArrayAdapter<String> posti= new ArrayAdapter<String>(OffriPassaggi.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
@@ -57,16 +55,13 @@ public class OffriPassaggi extends AppCompatActivity implements DatePickerDialog
         c.set(Calendar.MONTH,month);
         c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
         String currant_date= DateFormat.getDateInstance().format(c.getTime());
-        TextView t= findViewById(R.id.txtData);
-        set_data= findViewById(R.id.txtData);
-        set_data.setText(currant_date);
+        imp_data.setText(currant_date);
     }
     @Override
     public void onTimeSet(TimePicker timePicker, int hourOfday, int minute) {
         Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY,hourOfday);
         c.set(Calendar.MINUTE,minute);
-        set_ora=findViewById(R.id.txtOra);
-        set_ora.setText("  "+hourOfday+ " : "+ minute);
+        imp_ora.setText("  "+hourOfday+ " : "+ minute);
     }
 }
