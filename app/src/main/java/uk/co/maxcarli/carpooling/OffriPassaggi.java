4cd8a1766666de7d277544b49e7148b23ebece72
domenicoitps <domenicofasciano1996@libero.it>
2018-05-30 17:52:16 +0200

    data

--- app/src/main/java/uk/co/maxcarli/carpooling/OffriPassaggi.java
+++ app/src/main/java/uk/co/maxcarli/carpooling/OffriPassaggi.java
@@ -1,8 +1,11 @@
 package uk.co.maxcarli.carpooling;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TimeFormatException;
@@ -14,14 +17,27 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class OffriPassaggi extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
     EditText imp_data;
     EditText imp_ora;
     EditText set_auto;
     Spinner set_posti;
+    static String urlOffriPassaggio ="http://carpoolingsms.altervista.org/PHP/OffriPassaggi.php";
+    static AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,4 +80,57 @@ public class OffriPassaggi extends AppCompatActivity implements DatePickerDialog
         c.set(Calendar.MINUTE,minute);
         imp_ora.setText("  "+hourOfday+ " : "+ minute);
     }
+
+
+    public static void OffriPassaggi(final String date, final String time,final String car,final int place_avaiable,final String choise ,final Context context){
+        builder = new AlertDialog.Builder(context);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlOffriPassaggio,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        builder.setTitle("Server Response");
+                        builder.setMessage("Response"+ response);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        AlertDialog alertDialog= builder.create();
+                        alertDialog.show();
+                    }
+                }
+
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(context, "Error...", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String,String> params = new HashMap<String, String>();
+                params.put("date",date);
+                params.put("time", time);
+                params.put("car", car);
+                params.put("place_avaible", new String(String.valueOf(place_avaiable)));
+                params.put("choise", choise);
+
+                return params;
+            }
+        };
+
+
+        MySingleton.getmInstance(context).addTorequestque(stringRequest);
+
+
+
+    }
+
 }
