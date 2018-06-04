e87df52fe7ce7e559f64e62ed783182e9546034f
matera18 <33933065+matera18@users.noreply.github.com>
2018-06-04 17:30:06 +0200

    barra

---
+++ app/src/main/java/uk/co/maxcarli/carpooling/cerca_passaggio.java
@@ -0,0 +1,45 @@
+package uk.co.maxcarli.carpooling;
+
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class cerca_passaggio extends AppCompatActivity  {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cerca_passaggio);
+
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.cerca_passaggio);
+        mToggle=new ActionBarDrawerToggle(this, mDrawerLayout,  R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
