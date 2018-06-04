a5e8214727af14bfcd0be9d262c6e8239a1e2054
pansinisalvatore <rino.96@hotmail.it>
2018-06-04 15:49:24 +0200

    Activity di recupero password semi-completata

--- app/src/main/java/uk/co/maxcarli/carpooling/RecuperaPassword.java
+++ app/src/main/java/uk/co/maxcarli/carpooling/RecuperaPassword.java
@@ -1,13 +1,89 @@
 package uk.co.maxcarli.carpooling;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class RecuperaPassword extends AppCompatActivity {
+
+    String email;
+    List<String> responseList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recupera_password);
     }
+
+    public void leggiEmail(View view) {
+        String url = "http://carpoolingsms.altervista.org/PHP/RecuperoPassword.php";
+        EditText aux = (EditText) this.findViewById(R.id.emailRecupero);
+        final String EmailPassata = aux.getText().toString();
+       final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        builder.setTitle(R.string.emailInviataSuccesso);
+                        if (response.equals("1"))
+                        builder.setMessage(R.string.emailInviataSuccessoTesto);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                }
+
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("EmailPassata", EmailPassata);
+                return params;
+            }
+        };
+
+
+        MySingleton.getmInstance(this).addTorequestque(stringRequest);
+
+
+    }
+
 }
+
