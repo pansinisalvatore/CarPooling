2b1c0563dd498b71f4cef6549562683b825e9ac3
Gianni996 <giammaria.massafra@gmail.com>
2018-06-18 10:58:57 +0200

    Mappa quasi aggiustata

--- app/src/main/java/uk/co/maxcarli/carpooling/MappaCercaPassaggi.java
+++ app/src/main/java/uk/co/maxcarli/carpooling/MappaCercaPassaggi.java
@@ -1,27 +1,47 @@
 package uk.co.maxcarli.carpooling;
 
+import android.content.Context;
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MappaCercaPassaggi extends AppCompatActivity  implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MappaCercaPassaggi extends AppCompatActivity  implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
 
+    private String indirizzoCittadino;
+    private static final ArrayList<String> indirizzi=new ArrayList<String>();
+    private static final ArrayList<String> automobilisti=new ArrayList<String>();
+    private Address center;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,40 +61,35 @@ public class MappaCercaPassaggi extends AppCompatActivity  implements OnMapReady
         mMap = googleMap;
 
         double raggio=500;
-        Address a=getLocationFromAddress("Via Sparano 10, Bari");
+        center=getLocationFromAddress("Via Sparano 10, Bari");
         Address b=getLocationFromAddress("Via Argiro 10, Bari");
         Address c=getLocationFromAddress("Via Sagarriga Visconti 10, Bari");
+        getIndirizziPassaggiOfferti("21/3/2013","21.10",this);
 
         mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(a.getLatitude(),a.getLongitude()))
+                .position(new LatLng(center.getLatitude(),center.getLongitude()))
                 .title("La tua casa")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 
 
 
         mMap.addCircle(new com.google.android.gms.maps.model.CircleOptions()
-                .center(new LatLng(a.getLatitude(),a.getLongitude()))
+                .center(new LatLng(center.getLatitude(),center.getLongitude()))
                 .radius(raggio)
         );
 
-        if(controllo(a,b,raggio)){
-            mMap.addMarker(new MarkerOptions().
-                    position(new LatLng(b.getLatitude(),b.getLongitude())
-                    ).title("La casa di un altro").
-                    icon(BitmapDescriptorFactory
-                            .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        for(int i=0;i<indirizzi.size();i++){
+            Address pos=getLocationFromAddress(indirizzi.get(i));
+            if(controllo(center, pos, 500)){
+                mMap.addMarker(new MarkerOptions().
+                        position(new LatLng(pos.getLatitude(),pos.getLongitude())
+                        ).title("Automobilista: "+automobilisti.get(i)).
+                        icon(BitmapDescriptorFactory
+                                .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+            }
         }
 
-
-        if(controllo(a,c,raggio)){
-            mMap.addMarker(new MarkerOptions().
-                    position(new LatLng(c.getLatitude(),c.getLongitude())
-                    ).title("La casa di un altro").
-                    icon(BitmapDescriptorFactory
-                            .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-        }
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(a.getLatitude(),a.getLongitude()),18.0f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(center.getLatitude(),center.getLongitude()),18.0f));
 
 
     }
@@ -113,5 +128,81 @@ public class MappaCercaPassaggi extends AppCompatActivity  implements OnMapReady
         }
     }
 
+    private void getIndirizziPassaggiOfferti(final String data, final String ora, final Context context) {
+        String url = "http://carpoolingsms.altervista.org/PHP/PrendiIndirizzi.php";
 
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+
+                            JSONArray jsonarray = new JSONArray(response);
+
+                            for (int i = 0; i < jsonarray.length(); i++) {
+
+                                JSONObject jsonobject = jsonarray.getJSONObject(i);
+
+                                String indirizzo = jsonobject.getString("ResidenzaCittadino");
+                                indirizzi.add(indirizzo);
+                                String cognome=jsonobject.getString("CognomeCittadino");
+                                String nome=jsonobject.getString("NomeCittadino");
+                                automobilisti.add(cognome+" "+nome);
+                                Address pos=getLocationFromAddress(indirizzi.get(i));
+                                if(controllo(center, pos, 500)){
+                                    mMap.addMarker(new MarkerOptions().
+                                            position(new LatLng(pos.getLatitude(),pos.getLongitude())
+                                            ).title("Automobilista: "+automobilisti.get(i)).snippet("Clicca per prenotare").
+                                            icon(BitmapDescriptorFactory
+                                                    .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                                }
+                                Toast.makeText(context.getApplicationContext(),indirizzo,Toast.LENGTH_LONG).show();
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error != null) {
+
+                            Toast.makeText(context.getApplicationContext(), "Something went wrong.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("data", data);
+                params.put("ora", ora);
+                return params;
+            }
+        };
+
+        MySingleton.getmInstance(context.getApplicationContext()).addTorequestque(stringRequest);
+    }
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        String automobilista=marker.getTitle();
+
+
+        return false;
+    }
+
+
+    public void PrenotaPassaggio(){
+
+    }
 }
