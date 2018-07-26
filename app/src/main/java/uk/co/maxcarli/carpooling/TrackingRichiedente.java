f43637fb23ae4f9492183d517b6913bc53903843
pansinisalvatore <rino.96@hotmail.it>
2018-07-26 16:33:01 +0200

    agg bluetooth al richiedente

--- app/src/main/java/uk/co/maxcarli/carpooling/TrackingRichiedente.java
+++ app/src/main/java/uk/co/maxcarli/carpooling/TrackingRichiedente.java
@@ -1,6 +1,7 @@
 package uk.co.maxcarli.carpooling;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothAdapter.*;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,6 +9,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import static uk.co.maxcarli.carpooling.Control.ControlBluetooth.*;
 
 
@@ -17,32 +21,44 @@ public class TrackingRichiedente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking_richiedente);
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);
 
+
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter.isDiscovering()) {
+                Log.d("startDiscovery", "true");
+                mBluetoothAdapter.cancelDiscovery();
+            }
+                mBluetoothAdapter.startDiscovery();
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(mReceiver, filter);
 
     }
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+      BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
+           final String action = intent.getAction();
+            Log.d("action","ciao come stai");
+            Toast.makeText(TrackingRichiedente.this, "ciaoComeStai", Toast.LENGTH_LONG).show();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 String deviceName = device.getName();
                 String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d("deviceName", deviceName);
+                Log.d("deviceHardwareAddress", deviceHardwareAddress);
+                Toast.makeText(TrackingRichiedente.this, deviceName, Toast.LENGTH_LONG).show();
             }
         }
     };
 
-    public void listaDispositivi(BluetoothAdapter mBluetoothAdapter){
+    public void listaDispositiviAccoppiati(BluetoothAdapter mBluetoothAdapter){
 
         if (bluetoothIsActive(mBluetoothAdapter) == true){
             dispositiviAccoppiati(mBluetoothAdapter);
         }
     }
-
+/*
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -52,5 +68,5 @@ public class TrackingRichiedente extends AppCompatActivity {
         unregisterReceiver(mReceiver);
     }
 
-
+*/
 }
